797881fb26985bc30013c38f014b53e000a2377f
damarsimple <damaralbaribin@gmail.com>
2020-07-29 17:46:35 +0700

    Relational relationships between user and usercart

--- app/User.php
+++ app/User.php
@@ -37,4 +37,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function usercart()
+    {
+        return $this->hasMany(Usercart::class);
+    }
+
 }
