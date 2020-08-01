14e6625c4ce4ba0002483be23bfbfda2bf35830e
damarsimple <damaralbaribin@gmail.com>
2020-08-01 15:06:48 +0700

    fix wrong adress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 @section('content')
-            <section class="d-flex align-items-center position-relative vh-100 cover showcase coolbg">
-                <div class="container-fluid text-center">
-                        <div class="row">
-                            <div class="col-sm">
-                            <h1 class="text-white">Join Us in our crusade against the evil Ender Dragon !</h1>
-                            <button type="button" class="mt-2 btn btn-primary btn-lg">Join</button>
-                            </div>
-                        </div>
-                </div>
-            </section>
-            <div id="LandingCount"></div>
+<section class="d-flex align-items-center position-relative vh-100 cover showcase coolbg">
+    <div class="container-fluid text-center">
+        <div class="row">
+            <div class="col-sm">
+                <h1 class="text-white">Join Us in our crusade against the evil Ender Dragon !</h1>
+                <a href="/register"><button type="button" class="mt-2 btn btn-primary btn-lg">Join</button></a>
+            </div>
+        </div>
+    </div>
+</section>
+<div id="LandingCount"></div>
 @endsection
 @section('scripts')
 @endsection
