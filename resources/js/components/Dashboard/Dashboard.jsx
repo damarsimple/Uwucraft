b281c86d9e5e1abc3f4ebd9f5b655243cdfe8479
damarsimple <damaralbaribin@gmail.com>
2020-08-01 20:16:48 +0700

    fixed bug where comments always go to first child

--- resources/js/components/Dashboard/Dashboard.jsx
+++ resources/js/components/Dashboard/Dashboard.jsx
@@ -4,6 +4,8 @@ import ShopProfile from "../Shop/ShopProfile";
 import PostCard from "./PostCard";
 import Echo from "laravel-echo";
 import socketio from "socket.io-client";
+import InfiniteScroll from "react-infinite-scroll-component";
+import PropagateLoader from "react-spinners/ScaleLoader";
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
@@ -34,24 +36,32 @@ class Dashboard extends React.Component {
             this.setState({ totalPages });
             this.setState({ total });
             this.setState({ perpage });
+
+            const echo = new Echo({
+                broadcaster: "socket.io",
+                host: window.location.hostname + ":6001",
+                client: socketio
+            });
+
+            echo.channel("posts").listen("PostEvent", e => {
+                let index = this.state.posts.findIndex(
+                    obj => obj.id == e.data.post_id
+                );
+                if (index != -1) {
+                    //prepare the result
+                    let result = this.state.posts[index].comment.concat(e.data);
+                    let posts = this.state.posts;
+                    //Assign new data to object
+                    let comments = Object.assign({}, posts[index]);
+                    comments.comment = result;
+                    //assign new object to posts
+                    posts[index] = comments;
+                    this.setState({ posts: posts });
+                }
+            });
         });
-        // const currentPage = res.data.current_page;
-        // const nextPage = res.data.next_page_url;
-        // const lastPage = res.data.last_page_url;
-        // const totalPages = res.data.last_page;
-        // const total = parseInt(res.data.total);
-        // const perpage = res.data.per_page;
-        // const posts = res.data.data;
-        // this.setState({ posts });
-        // this.setState({ currentPage });
-        // this.setState({ nextPage });
-        // this.setState({ lastPage });
-        // this.setState({ totalPages });
-        // this.setState({ total });
-        // this.setState({ perpage });
     }
     fetchMoreData() {
-        const number = parseInt(this.state.currentPage) + 1;
         axios.get(this.state.nextPage).then(res => {
             const currentPage = res.data.current_page;
             const posts = this.state.posts.concat(res.data.data);
@@ -70,13 +80,22 @@ class Dashboard extends React.Component {
             <div className="container">
                 <div className="row padding">
                     <div className="col-md-9">
-                        {/** Left */}
-                        {this.state.posts.map((post, index) => {
-                            return <PostCard key={index} data={post} />;
-                        })}
-
-                        {/** End Loop Here */}
-                        {/** EndLeft */}
+                        <InfiniteScroll
+                            dataLength={this.state.posts.length}
+                            next={this.fetchMoreData.bind(this)}
+                            hasMore={this.state.hasMore}
+                            loader={
+                                <PropagateLoader
+                                    size={150}
+                                    color={"#9B9B9B"}
+                                    loading={true}
+                                />
+                            }
+                        >
+                            {this.state.posts.map((post, index) => {
+                                return <PostCard key={index} props={post} />;
+                            })}
+                        </InfiniteScroll>
                     </div>
                     <div className="col-md-3">
                         <ShopProfile
--- resources/js/components/Dashboard/PostCard.jsx
+++ resources/js/components/Dashboard/PostCard.jsx
@@ -1,138 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaEllipsisH } from "react-icons/fa";
 import { MdSend } from "react-icons/md";
 import Echo from "laravel-echo";
 import socketio from "socket.io-client";
-
-export default class PostCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: "",
-            button: false,
-
-            username: this.props.data.user.username,
-            content: this.props.data.content,
-            comment: this.props.data.comment,
-            reaction: this.props.data.reaction,
-
+export default function PostCard(props) {
+    const data = props.props;
+    const [input, setInput] = useState("");
+    const [button, setButton] = useState(false);
+    const submit = () => {
+        const sub = {
+            id: data.id,
+            content: input,
         };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    componentDidMount() {
-        const e = new Echo({
-            broadcaster: "socket.io",
-            host: window.location.hostname + ":6001",
-            client: socketio,
-        });
-        /** Init Echo  */
-        e.channel(`posts.${this.props.data.id}`).listen("PostEvent", (e) => {
-            console.log(e.data);
-            this.setState({comment: this.state.comment.concat(e.data)})
-        });
-    }
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
-    handleMouseOver() {
-        this.setState({ button: !this.state.button });
-    }
-    handleSubmit(event) {
-        const data = {
-            id: this.props.data.id,
-            content: this.state.value,
-        };
-        axios.post("/ajax/posts", data);
-        this.setState({ value: ""});
-    }
-    render() {
-        return (
-            <>
-                <div className="instagram-card">
-                    <div className="instagram-card-header">
-                        <img
-                            src="https://randomuser.me/api/portraits/women/46.jpg"
-                            className="instagram-card-user-image"
-                        />
-                        <a
-                            className="instagram-card-user-name"
-                            href={`./${this.state.username}`}
-                        >
-                            {this.state.username}
-                        </a>
-                        <div className="instagram-card-action d-flex justify-content-center">
-                            <FaEllipsisH />
-                        </div>
-                    </div>
-                    <div className="intagram-card-image mt-4">
-                        <img
-                            className="img-fluid"
-                            width="960px"
-                            src="https://source.unsplash.com/random"
-                        />
-                    </div>
-                    <div className="instagram-card-content">
-                        <p className="likes">
-                            {this.state.reaction.length} reactions
-                        </p>
-                        <p>
-                            <a
-                                className="instagram-card-content-user"
-                                href={`./${this.state.username}`}
-                            >
-                                {this.state.username}
-                            </a>
-                            {this.state.content}
-                        </p>
-                        <p className="comments">
-                            view all {this.state.comment.length} comments
-                        </p>
-                        {this.state.comment.map((comment, i) => {
-                            return (
-                                <div key={i}>
-                                    <a
-                                        href={`./${comment.user.username}`}
-                                        className="user-comment"
-                                    >
-                                        {comment.user.username}
-                                    </a>
-                                    {comment.content}
-                                </div>
-                            );
-                        })}
-                        <br />
-                        <div className="instagram-card-time">58 min</div>
-                        <br />
-                        <hr />
-                    </div>
-                    <div className="instagram-card-footer">
-                        <a className="footer-action-icons" href="#">
-                            <i className="fa fa-heart-o" />
-                        </a>
-
-                        <input
-                            className="comments-input"
-                            type="text"
-                            placeholder="Add a comment ..."
-                            value={this.state.value}
-                            onChange={this.handleChange}
-                        />
-
-                        <MdSend
-                            fill={this.state.button ? "blue" : ""}
-                            onMouseEnter={this.handleMouseOver.bind(this)}
-                            onMouseLeave={this.handleMouseOver.bind(this)}
-                            size="2em"
-                            onClick={this.handleSubmit}
-                        />
-                        <a className="footer-action-icons" href="#"></a>
+        console.log(sub);
+        setInput('');
+        return axios.post("/ajax/posts", sub);
+    };
+    return (
+        <>
+            <div className="instagram-card">
+                <div className="instagram-card-header">
+                    <img
+                        src="https://randomuser.me/api/portraits/women/46.jpg"
+                        className="instagram-card-user-image"
+                    />
+                    <a
+                        className="instagram-card-user-name"
+                        href={`./${data.user.username}`}
+                    >
+                        {data.user.username}
+                    </a>
+                    <div className="instagram-card-action d-flex justify-content-center">
+                        <FaEllipsisH />
                     </div>
                 </div>
-                <br />
-                <br />
-            </>
-        );
-    }
+                <div className="intagram-card-image mt-4">
+                    <img
+                        className="img-fluid"
+                        width="960px"
+                        src="https://source.unsplash.com/random"
+                    />
+                </div>
+                <div className="instagram-card-content">
+                    <p className="likes">{data.reaction.length} reactions</p>
+                    <p>
+                        <a
+                            className="instagram-card-content-user"
+                            href={`./${data.user.username}`}
+                        >
+                            {data.user.username}
+                        </a>
+                        {data.content}
+                    </p>
+                    <p className="comments">
+                        view all {data.comment.length} comments
+                    </p>
+                    {data.comment.map((comment, i) => {
+                        return (
+                            <div key={i}>
+                                <a
+                                    href={`./${comment.user.username}`}
+                                    className="user-comment"
+                                >
+                                    {comment.user.username}
+                                </a>
+                                {comment.content}
+                            </div>
+                        );
+                    })}
+                    <br />
+                    <div className="instagram-card-time">58 min</div>
+                    <br />
+                    <hr />
+                </div>
+                <div className="instagram-card-footer">
+                    <a className="footer-action-icons" href="#">
+                        <i className="fa fa-heart-o" />
+                    </a>
+
+                    <input
+                        className="comments-input"
+                        type="text"
+                        placeholder="Add a comment ..."
+                        value={input}
+                        onChange={e => setInput(e.target.value)}
+                    />
+
+                    <MdSend
+                        fill={button ? "blue" : ""}
+                        onMouseEnter={() => setButton(!button)}
+                        onMouseLeave={() => setButton(!button)}
+                        size="2em"
+                          onClick={submit}
+                    />
+                    <a className="footer-action-icons" href="#"></a>
+                </div>
+            </div>
+            <br />
+            <br />
+        </>
+    );
 }
