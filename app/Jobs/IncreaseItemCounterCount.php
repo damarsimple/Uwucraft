972e0c5bc9bc9d02d668bdbf8d4fb3468e8f7eb7
damarsimple <damaralbaribin@gmail.com>
2020-08-29 02:05:19 +0700

    jobs to increase item view

---
+++ app/Jobs/IncreaseItemCounterCount.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class IncreaseItemCounterCount implements ShouldQueue
+{
+    protected $item;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Item $item)
+    {
+        $this->item = $item;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->item->increment('counter', 1);
+    }
+}
