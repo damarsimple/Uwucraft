dbb15d4f420c030f3191d3370eeed97eabb0f047
Damar Albaribin <damaralbaribin@gmail.com>
2020-09-08 13:51:13 +0000

    Moving to TSX

--- resources/js/components/Echo.jsx
+++
@@ -1,9 +0,0 @@
-import Echo from "laravel-echo";
-import socketio from "socket.io-client";
-/** Init Echo  */
-const echo = new Echo({
-    broadcaster: "socket.io",
-    host: window.location.hostname + ":6001", // this is laravel-echo-server host
-    client: socketio
-});
-export default echo;
