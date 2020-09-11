0d9d466cfc0552d3b9cfb7cde2117d57fd16434b
Damar Albaribin <damaralbaribin@gmail.com>
2020-09-11 11:50:46 +0000

    working scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,7 @@
-# importing the libraries
 from bs4 import BeautifulSoup
 import requests
 import csv
+
 url = "https://minecraft.gamepedia.com/Advancement"
 
 # Make a GET request to fetch the raw HTML content
@@ -9,53 +9,35 @@ html_content = requests.get(url).text
 
 # Parse the html content
 soup = BeautifulSoup(html_content, "lxml")
-
-gdp_table = soup.find("table", attrs={"data-description": "advancements"})
-gdp_table_data = gdp_table.tbody.find_all("tr")  # contains 2 rows
-
-# Get all the headings of Lists
-headings = []
-for td in gdp_table_data[0].find_all("td"):
-    # remove any newlines and extra spaces from left and right
-    headings.append(td.b.text.replace('\n', ' ').strip())
-
-
 data = {}
-for table, heading in zip(gdp_table_data[1].find_all("table"), headings):
-    # Get headers of table i.e., Rank, Country, GDP.
-    t_headers = []
-    for th in table.find_all("th"):
-        # remove any newlines and extra spaces from left and right
-        t_headers.append(th.text.replace('\n', ' ').strip())
-    # Get all the rows of table
-    table_data = []
-    for tr in table.tbody.find_all("tr"):  # find all tr's from table's tbody
-        t_row = {}
-        # Each table row is stored in the form of
-        # t_row = {'Rank': '', 'Country/Territory': '', 'GDP(US$million)': ''}
+csv_writer = csv.writer(open('test2.csv', mode='w'), delimiter=',',
+                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
+advancement_table = soup.find_all(
+    "table", attrs={"class": "wikitable", "data-description": "advancements"})
+headings = []
+data = []
 
-        # find all td's(3) in tr and zip it with t_header
-        for td, th in zip(tr.find_all("td"), t_headers):
-            t_row[th] = td.text.replace('\n', '').strip()
-        table_data.append(t_row)
+advancement_table_data = advancement_table[1].tbody.find_all("tr")
 
-    # Put the data for the table with his heading.
-    data[heading] = table_data
+for th in advancement_table_data[1].find_all("th"):
+    headings.append(th.text.replace("\n", " ").strip())
+print(headings)
+csv_writer.writerow(headings)
+for table in advancement_table:
+    advancement_table_data = table.tbody.find_all("tr")
 
+    for th in advancement_table_data[1].find_all("th"):
+        headings.append(th.text.replace("\n", " ").strip())
 
-for topic, table in data.items():
-    # Create csv file for each table
-    with open(f"{topic}.csv", 'w') as out_file:
-        # Each 3 table has headers as following
-        headers = [
-            "Country/Territory",
-            "GDP(US$million)",
-            "Rank"
-        ]  # == t_headers
-        writer = csv.DictWriter(out_file, headers)
-        # write the header
-        writer.writeheader()
-        for row in table:
-            if row:
-                writer.writerow(row)
-print(data)
+    for table in advancement_table_data:
+        if table.td is None:
+            print("none")
+        else:
+            subtab = table.find_all("td")
+            tabdata = []
+            for tab in subtab:
+                tabdata.append(tab.text.replace("\n", " ").strip())
+            data.append(tabdata)
+
+for d in data:
+    csv_writer.writerow(d)
